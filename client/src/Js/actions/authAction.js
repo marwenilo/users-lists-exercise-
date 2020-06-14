f0de966ccc4157519e7cc98875bba149cadb9e9c
Marwen Magri <marwen.magri@gomycode.co>
2020-06-14 17:30:57 +0100

    cleaning code + checking on user exist

--- client/src/Js/actions/authAction.js
+++ client/src/Js/actions/authAction.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alertAction";
 import {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
@@ -60,38 +58,6 @@ export const login = (name, family_name, password, loginTime) => async (
 
     dispatch({
       type: LOGIN_FAIL,
-    });
-  }
-};
-
-// Register User
-export const register = ({ firstName, lastName, email, password }) => async (
-  dispatch
-) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  const body = JSON.stringify({ firstName, lastName, email, password });
-
-  try {
-    const res = await axios.post("/api/users", body, config);
-
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data,
-    });
-  } catch (err) {
-    const errors = err.response.data.errors;
-
-    if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
-    }
-
-    dispatch({
-      type: REGISTER_FAIL,
     });
   }
 };
--- client/src/Js/reducers/authReducer.js
+++ client/src/Js/reducers/authReducer.js
@@ -1,5 +1,4 @@
 import {
-  REGISTER_SUCCESS,
   USER_LOADED,
   LOGIN_SUCCESS,
   LOGOUT,
@@ -22,14 +21,6 @@ export default function (state = initialState, action) {
         isAuthenticated: true,
         loading: false,
         user: payload,
-      };
-    case REGISTER_SUCCESS:
-      localStorage.setItem("token", payload.token);
-      return {
-        ...state,
-        ...payload,
-        isAuthenticated: true,
-        loading: false,
       };
     case LOGIN_SUCCESS:
       localStorage.setItem("token", payload.token);
